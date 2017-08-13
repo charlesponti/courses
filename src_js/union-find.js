98a106acf76da5c1b7958f1d2949d5a695d44f1b
🇵🇷 <cj@ponti.io>
2017-08-13 20:20:20 +0100

    feat(union-find): Add initial file for Union-Find with template functions

---
+++ src_js/union-find.js
@@ -0,0 +1,45 @@
+/**
+ * Initialise union-find data structure with N objects
+ * (0 - N-1)
+ */
+function unionFind(n) {
+
+}
+
+/**
+ * Add connection between two objects
+ * @param {Number} p 
+ * @param {Number} q 
+ * @returns {undefined}
+ */
+function union(p, q) {
+
+}
+
+/**
+ * Check for connection between two objects. Are they in the same
+ * connected component?
+ * @param {Number} p 
+ * @param {Number} q 
+ * @returns {Boolean}
+ */
+function connected(p, q) {
+
+}
+
+/**
+ * Component identifier for p(0 to N-1)
+ * @param {*} p 
+ * @return {Number}
+ */
+function find(p) {
+
+}
+
+/**
+ * Number of connected components
+ * @returns {Number}
+ */
+function count() {
+
+}
